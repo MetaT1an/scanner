d7c57dd685bb7f2123d6c04591d4eb0843f7b078
Yuchen Tian <tyc896@qq.com>
2019-04-13 13:09:43 +0800

    [bug fixed] fix url problem

--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -7,5 +7,5 @@ if [[ $? -eq 1 ]];then
     pipenv --rm
 fi
 
-# remove repository, this shell should be executed in the same directory as scanner/
-rm -rf scanner
+# remove repository, this shell should be executed in the scanner/ directory
+rm -rf ../scanner
