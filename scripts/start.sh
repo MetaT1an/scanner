fbd337b4384cee9a79ce69289fea817f776e1ce9
Yuchen Tian <tyc896@qq.com>
2019-04-16 19:08:26 +0800

    [bug fixed] thanks fabric for making my scripts so ugly

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # to check whether the environment has been created
-pipenv --venv
+~/.local/bin/pipenv --venv
 
 # if not, create
 if [[ $? -eq 1 ]];then
-    pipenv --python python3 install
+    ~/.local/bin/pipenv --python python3 install
 fi
 
-# start a celery task in virtual environment
-pipenv run celery -A celery_task worker -l info --workdir main/
+# start a celery task in virtual environment in background, thus
+~/.local/bin/pipenv run celery -A celery_task worker -l info --workdir main/ &
+
+# thanks fabric for making my shell script so ugly!
+# https://github.com/fabric/fabric/issues/1901
--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # remove virtual environment
-pipenv --venv
+~/.local/bin/pipenv --venv
 
 if [[ $? -eq 0 ]];then
-    pipenv --rm
+    ~/.local/bin/pipenv --rm
 fi
 
 # remove repository, this shell should be executed in the scanner/ directory
 rm -rf ../scanner
+
+# thanks fabric for making my shell script so ugly!
+# https://github.com/fabric/fabric/issues/1901
